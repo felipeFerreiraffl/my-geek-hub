1b715476438fc2f716052f2ca51f6ba08018ddfc
felipeFerreiraffl <felipado2x0@gmail.com>
2026-03-23 21:38:38 -0300

    chore: infered types addition for sql tables

---
+++ backend/src/types/db.types.ts
@@ -0,0 +1,13 @@
+import { bookmarks, ratings, users } from "@/db/drizzle/index.js";
+import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+
+export type User = InferSelectModel<typeof users>;
+export type NewUser = InferInsertModel<typeof users>;
+
+export type Bookmark = InferSelectModel<typeof bookmarks>;
+export type NewBookmark = InferInsertModel<typeof bookmarks>;
+
+export type Rating = InferSelectModel<typeof ratings>;
+export type NewRating = InferInsertModel<typeof ratings>;
+
+export type SafeUser = Omit<User, "password">;
